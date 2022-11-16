abd9b09dcb75704ddfeba30aaa9459330e3ecb2c
Alessia B <borys.alessia@gmail.com>
2022-11-16 16:43:25 +0100

    prev dogs working

--- src/App.tsx
+++ src/App.tsx
@@ -2,56 +2,24 @@ import { useState } from "react";
 import "./style.css"
 
 interface Dog {
-  // id: number
   message: string;
   status: string;
 }
 
 function App() {
   const [dog, setDog] = useState<Dog>();
-
-  const previousDogs: Dog[]= []
+  const [prev, setPrev] = useState<Dog[]>([])
 
   const handleGetDog = async () => {
     const response = await fetch("https://dog.ceo/api/breeds/image/random");
     const jsonBody: Dog = await response.json();
-    setDog(jsonBody);
+    setDog(jsonBody)
     if (dog !== undefined){
-      previousDogs.unshift(dog)
+      setPrev(current => [dog, ...current]);
     }
   };
 
-  // const handleHistory = () => {
-  //   setDog(dog);
-  //   if (dog !== undefined){
-  //     previousDogs.unshift(dog)
-  //     console.log(previousDogs.length)
-  //   }
-  // }
-
-  // const handleGetDog = () => {
-  //   fetch("https://dog.ceo/api/breeds/image/random")
-  //     .then((response) => response.json())
-  //     .then((jsonBody: Dog) => setDog(jsonBody));
-  // };
-
-  // interface DogMapProps {
-  //   dog: Dog;
-  // }
-  // const DogMap = (props:DogMapProps) => {
-  //   return <img src={props.dog.message} alt={props.dog.status} />
-  // };
-  // const DogList = () => {
-  //   return (
-  //     <p>
-  //       {previousDogs.map((dog) => {
-  //         return <DogMap dog={dog} key={dog.id}/>;
-  //       })}
-  //     </p>
-  //   );
-  // };
-
-  const PreviousDogs = previousDogs.map((dog) => {
+  const PreviousDogs = prev.map((dog) => {
     return <img src={dog.message} alt="" className="prev-image"/>
   })
 
@@ -59,15 +27,15 @@ function App() {
     return (
       <div>
         <h1>Dog app</h1>
-        <button onClick={handleGetDog}>Get another dog</button>
-        {/* <button onClick={() => {handleGetDog(); handleHistory();}}>Get another dog</button> */}
+        <button 
+          onClick={handleGetDog}
+        >Get another dog</button>
         <hr />
         <img src={dog.message} alt={dog.status} className="dog-image"/>
         <hr />
         <div>
           <h3>Previous dogs</h3>
           {PreviousDogs}
-          {/* <DogList /> */}
         </div>
       </div>
 
