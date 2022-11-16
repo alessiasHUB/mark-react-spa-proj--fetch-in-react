3544e5c175a38fb61565312b734924bba0e67c12
Alessia B <borys.alessia@gmail.com>
2022-11-16 16:17:57 +0100

    attempt at previous

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import "./style.css"
 
 interface Dog {
-  // id: number;
+  // id: number
   message: string;
   status: string;
 }
@@ -10,26 +10,67 @@ interface Dog {
 function App() {
   const [dog, setDog] = useState<Dog>();
 
-  // const handleGetDog = async () => {
-  //   const response = await fetch("https://dog.ceo/api/breeds/image/random");
-  //   const jsonBody: Dog = await response.json();
-  //   setDog(jsonBody);
+  const previousDogs: Dog[]= []
+
+  const handleGetDog = async () => {
+    const response = await fetch("https://dog.ceo/api/breeds/image/random");
+    const jsonBody: Dog = await response.json();
+    setDog(jsonBody);
+    if (dog !== undefined){
+      previousDogs.unshift(dog)
+    }
+  };
+
+  // const handleHistory = () => {
+  //   setDog(dog);
+  //   if (dog !== undefined){
+  //     previousDogs.unshift(dog)
+  //     console.log(previousDogs.length)
+  //   }
+  // }
+
+  // const handleGetDog = () => {
+  //   fetch("https://dog.ceo/api/breeds/image/random")
+  //     .then((response) => response.json())
+  //     .then((jsonBody: Dog) => setDog(jsonBody));
   // };
 
-  const handleGetDog = () => {
-    fetch("https://dog.ceo/api/breeds/image/random")
-      .then((response) => response.json())
-      .then((jsonBody: Dog) => setDog(jsonBody));
-  };
+  // interface DogMapProps {
+  //   dog: Dog;
+  // }
+  // const DogMap = (props:DogMapProps) => {
+  //   return <img src={props.dog.message} alt={props.dog.status} />
+  // };
+  // const DogList = () => {
+  //   return (
+  //     <p>
+  //       {previousDogs.map((dog) => {
+  //         return <DogMap dog={dog} key={dog.id}/>;
+  //       })}
+  //     </p>
+  //   );
+  // };
+
+  const PreviousDogs = previousDogs.map((dog) => {
+    return <img src={dog.message} alt="" className="prev-image"/>
+  })
 
   if (dog) {
     return (
       <div>
         <h1>Dog app</h1>
         <button onClick={handleGetDog}>Get another dog</button>
+        {/* <button onClick={() => {handleGetDog(); handleHistory();}}>Get another dog</button> */}
         <hr />
-        <img src={dog.message} alt="a dog" className="dog-image"/>
+        <img src={dog.message} alt={dog.status} className="dog-image"/>
+        <hr />
+        <div>
+          <h3>Previous dogs</h3>
+          {PreviousDogs}
+          {/* <DogList /> */}
+        </div>
       </div>
+
     );
   } else {
     return (
